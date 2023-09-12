a8475ca842d061dc8652d0e5b12d77d4c053b180
Munmun Bhuin <144488862+munmun08@users.noreply.github.com>
2023-09-12 19:33:45 +0530

    signup.js page modified

--- signup.js
+++ signup.js
@@ -1,21 +1,45 @@
-var newPass,confrPass;
-function setnewPass(){
-   newPass=document.querySelector('#setPass').value;
-}
-function setcnfrPass(){
-    confrPass=document.querySelector('#cnfrPass').value;
-}
-function checkvalid(){
-    if(newPass.length <8   || confrPass<8) {
-        alert("Please enter password of minimum 8 length");
+const form = document.getElementById('myForm');
+const usernameInput = document.getElementById('username');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirmPassword');
+const errorMessage = document.getElementById('error-message');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Basic username validation (minimum 4 characters)
+    const username = usernameInput.value;
+    if (!/^[a-zA-Z]{4,}$/.test(username)) {
+        errorMessage.textContent = "Username must be at least 4 characters long and contain only letters.";
         return;
     }
-    console.log(newPass);
-    console.log(confrPass);
-    if(newPass==confrPass){
-        alert("signup successful");
+
+    // Basic email validation
+    const email = emailInput.value;
+    const allowedExtensions = ["com", "in"]; // Add the extensions you want to allow
+    const emailRegex = new RegExp(`^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9.-]+\\.(${allowedExtensions.join('|')}))$`, 'i');
+
+    if (!emailRegex.test(email)) {
+        errorMessage.textContent = "Please enter a valid email address";
+        return;
     }
-    else{
-        alert("Password Mismatch");
+
+    // Basic password validation (minimum 6 characters)
+    const password = passwordInput.value;
+    if (password.length < 6) {
+        errorMessage.textContent = "Password must be at least 6 characters long.";
+        return;
     }
-}
+
+    // Check if the password and confirm password match
+    const confirmPassword = confirmPasswordInput.value;
+    if (password !== confirmPassword) {
+        errorMessage.textContent = "Passwords do not match.";
+        return;
+    }
+
+    // If all validations pass, allow the form to submit
+    errorMessage.textContent = '';
+    form.submit();
+});
