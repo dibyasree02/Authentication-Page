414166270a7cd728e7562a21d50f3ae1f7f934e9
anurupa-21 <115604000+anurupa-21@users.noreply.github.com>
2023-09-14 07:00:06 +0530

    Add files via upload

---
+++ forgot-password.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from 'axios'
+
+
+function ForgotPassword() {
+    const [email, setEmail] = useState()
+    const navigate = useNavigate()
+
+    axios.defaults.withCredentials = true;
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3001/forgot-password', {email})
+        .then(res => {
+            if(res.data.Status === "Success") {
+                navigate('/login')
+               
+            }
+        }).catch(err => console.log(err))
+    }
+
+    return(
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h4>Forgot Password</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              autoComplete="off"
+              name="email"
+              className="form-control rounded-0"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Send
+          </button>
+          </form>
+        
+      </div>
+    </div>
+    )
+}
+
+export default ForgotPassword;
